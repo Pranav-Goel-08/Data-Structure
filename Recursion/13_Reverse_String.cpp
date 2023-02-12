e46aca4b322fb56323324500eef87bb2ca03f0fa
Pranav-Goel-08 <Pranav.Goel.2024s@gmail.com>
2023-02-12 07:50:52 +0530

    Update 13_Reverse_String.cpp

--- Recursion/13_Reverse_String.cpp
+++ Recursion/13_Reverse_String.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
-void reverseString(string &s,int start,int end){
-    if(start>=end){
+
+// Method 1 : 
+
+// void reverseString(string &s,int start,int end){
+//     if(start>=end){
+//         return;
+//     }
+//     swap(s[start],s[end]);
+//     reverseString(s,start+1,end-1);
+// }
+
+// Method 2 :
+void reverseString(string &s,int index,int n){
+    if(index==n){
         return;
     }
-    swap(s[start],s[end]);
-    reverseString(s,start+1,end-1);
+    
+    reverseString(s,index+1,n);
+    cout<<s[index]<<" ";
 }
+
 int main(){
     int n ;
     cout<<"Enter the length of the string : ";
@@ -17,10 +31,14 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>s[i];
     }
-    int start = 0;
-    int end = n-1;
-    reverseString(s,start,end);
-    for(int i=0;i<n;i++){
-        cout<<s[i]<<" ";
-    }cout<<endl;
+    // Call for Method 1 :
+    // int start = 0;
+    // int end = n-1;
+    // reverseString(s,start,end);
+    // for(int i=0;i<n;i++){
+    //     cout<<s[i]<<" ";
+    // }cout<<endl;
+
+    // Call for Method 2
+    reverseString(s,0,n);
 }
