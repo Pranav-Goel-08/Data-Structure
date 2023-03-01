b775d81b26b7c5388e672e60c755ae363abc769d
Pranav-Goel-08 <Pranav.Goel.2024s@gmail.com>
2023-03-01 10:26:32 +0530

    Update 509_Fibonacci_Number.cpp

--- LeetCode Problems/509_Fibonacci_Number.cpp
+++ LeetCode Problems/509_Fibonacci_Number.cpp
@@ -1,3 +1,5 @@
+// Memoization
+
 class Solution {
 public:
     int fibo(int n , vector<int> &dp){
@@ -18,3 +20,46 @@ public:
         return fibo(n,dp);
     }
 };
+
+
+/*
+
+Tabulation :
+
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(n+1,0);
+        for(int i=0;i<=n;i++){
+            if(i==0 || i==1){
+                dp[i] = i;
+            }
+            else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}; */
+
+/*
+Space Optimisation
+
+class Solution {
+public:
+    int fib(int n) {
+        int prev1 = 1;
+        int prev2 = 0;
+        if(n==0){
+            return 0;
+        }
+        for(int i=2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
+
+*/
